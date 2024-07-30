0cdba41e498b16e9502667b8daca08d97057eaf0
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-07-31 01:02:02 +0800

    Time: 381 ms (5.08%), Space: 19.2 MB (45.95%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,55 @@
+
+class Solution:
+    def BFS(self, grid, root):
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        vertices = [root]
+        queue = [root]
+
+        while queue:
+            i, j = queue.pop(0)
+
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if self.check_one(ni, nj, grid) and (ni, nj) not in vertices:
+                    self.add_to_lists((ni, nj), vertices, queue)
+        
+        return vertices
+
+    def check_one(self, i, j, grid):
+        # Check if the cell is within bounds and if it is '1'
+        return 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1'
+
+    def add_to_lists(self, element, list1, list2):
+        list1.append(element)
+        list2.append(element)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        water_surround_grid = []
+        found_lands = []
+
+        # Create a grid surrounded by water
+        for i in range(m + 2):
+            water_surround_grid.append([])
+            found_lands.append([])
+
+            for j in range(n + 2):
+                found_lands[i].append(False)
+
+                if j == 0 or j == (n + 1) or i == 0 or i == (m + 1):
+                    water_surround_grid[i].append("0")
+                else:
+                    water_surround_grid[i].append(grid[i - 1][j - 1])
+
+        islands = 0
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if water_surround_grid[i][j] == "1" and not found_lands[i][j]:
+                    islands += 1
+                    adjacent_lands = self.BFS(water_surround_grid, (i, j))
+                    for x, y in adjacent_lands:
+                        found_lands[x][y] = True
+
+        return islands
