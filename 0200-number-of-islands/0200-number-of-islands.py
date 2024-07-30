e6e0e8be858ddc825c6a9820af59d3ce613d2475
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-07-31 01:12:23 +0800

    Time: 385 ms (5.08%), Space: 19.3 MB (44.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,4 +1,3 @@
-
 class Solution:
     def BFS(self, grid, root):
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -15,9 +14,8 @@ class Solution:
         
         return vertices
 
-    def check_one(self, i, j, grid):
-        # Check if the cell is within bounds and if it is '1'
-        return 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1'
+    def check_one(self, i,j, grid):
+        return (grid[i][j] == '1')
 
     def add_to_lists(self, element, list1, list2):
         list1.append(element)
@@ -30,26 +28,27 @@ class Solution:
         water_surround_grid = []
         found_lands = []
 
-        # Create a grid surrounded by water
-        for i in range(m + 2):
+        for i in range(m+2):
             water_surround_grid.append([])
             found_lands.append([])
 
-            for j in range(n + 2):
+            for j in range(n+2):
                 found_lands[i].append(False)
 
-                if j == 0 or j == (n + 1) or i == 0 or i == (m + 1):
+                if j == 0 or  j == (n+1) or i == 0 or i == (m+1):
                     water_surround_grid[i].append("0")
                 else:
-                    water_surround_grid[i].append(grid[i - 1][j - 1])
-
+                    water_surround_grid[i].append(grid[i-1][j-1])
+  
         islands = 0
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
+        for i in range(1, m+1):
+            for j in range(1,n+1):
                 if water_surround_grid[i][j] == "1" and not found_lands[i][j]:
                     islands += 1
-                    adjacent_lands = self.BFS(water_surround_grid, (i, j))
-                    for x, y in adjacent_lands:
+
+                    adjenct_lands = self.BFS(water_surround_grid,(i, j))
+                    for x, y in adjenct_lands:
                         found_lands[x][y] = True
+                    
 
         return islands
