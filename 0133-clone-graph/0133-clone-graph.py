52106c6559a78ed5206557540f7abe8e4ed48b40
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-08-01 20:40:56 +0800

    Time: 44 ms (30.35%), Space: 17 MB (29.27%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -10,20 +10,20 @@ from typing import Optional
 class Solution:
     def clone_BFS(self, root_node, clone_root_node):
 	
-        queue = [(root_node, clone_root_node)]
+        queue = [root_node]
         clone_graph = {root_node.val: clone_root_node}
 
         while queue:
-            node, clone_node = queue.pop(0) 
+            node = queue.pop(0) 
 
             for neighbor_node in node.neighbors:
                 if neighbor_node.val not in clone_graph.keys():
                     clone_neighbor = Node(neighbor_node.val, [])
                     clone_graph[neighbor_node.val] = clone_neighbor
 
-                    queue.append((neighbor_node, clone_neighbor))
+                    queue.append(neighbor_node)
 
-                clone_node.neighbors.append(clone_graph[neighbor_node.val])
+                clone_graph[node.val].neighbors.append(clone_graph[neighbor_node.val])
 
 
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
