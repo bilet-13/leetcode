967789d265b952eccd7d0053682936d7405f2d94
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-08-01 20:25:12 +0800

    Time: 46 ms (19.22%), Space: 17 MB (29.27%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,37 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from copy import deepcopy
+from typing import Optional
+class Solution:
+    def clone_BFS(self, root_node, clone_root_node):
+	
+        queue = [(root_node, clone_root_node)]
+        clone_graph = {root_node.val: clone_root_node}
+
+        while queue:
+            node, clone_node = queue.pop(0) 
+
+            for neighbor_node in node.neighbors:
+                if clone_graph.get(neighbor_node.val, None) == None:
+                    clone_neighbor = Node(neighbor_node.val, [])
+                    clone_graph[neighbor_node.val] = clone_neighbor
+                    
+                    queue.append((neighbor_node, clone_neighbor))
+
+                clone_node.neighbors.append(clone_graph[neighbor_node.val])
+
+
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        
+        if not node:
+            return node
+
+        clone_root_node = Node(node.val, [])
+        self.clone_BFS(node, clone_root_node)
+        return clone_root_node
+        
