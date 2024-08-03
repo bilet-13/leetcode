3cbd309d16ac582a7ee71e3b6f267e3942e85b2f
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-08-03 21:10:02 +0800

    Time: 93 ms (38.38%), Space: 18.1 MB (97.92%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,37 +1,28 @@
 class Solution:
     def build_course_graph(self, numCourses, prerequisites):
-        graph = {}
-
-        for num in range(numCourses):
-            graph[num] = {'out':[], 'in':[]}
+        graph = {i:{'in':[], 'out': []} for i in range(numCourses)}
         
         for course, pre_course in prerequisites:
-            graph[pre_course]['out'].append(course)
             graph[course]['in'].append(pre_course)
+            graph[pre_course]['out'].append(course)
+            
         return graph
 
     def check_cycle(self, graph):
 
-        queue = []
-        visited = []
-        for node in graph.keys():
-            if not graph[node]['in']:
-                queue.append(node)
-                visited.append(node)
+        queue = deque([node for node in graph if not graph[node]['in']])
+        visited = set(queue)
 
         if not queue:
             return True
 
         while queue:
-            node = queue.pop(0)
-            for ele in graph.keys():
-                if node in graph[ele]['in']:
-                    graph[ele]['in'].remove(node)
-
-            for node in graph.keys():
-                if not graph[node]['in'] and node not in visited:
-                    queue.append(node)
-                    visited.append(node)
+            node = queue.pop()
+            for neighbor in graph[node]['out']:
+                graph[neighbor]['in'].remove(node)
+                if not graph[neighbor]['in']:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
 
             if not queue and len(visited) != len(graph.keys()):
                 return True
