114d2ad8f7bd32cc4c444566a1cacb6833713718
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-08-05 22:32:26 +0800

    Time: 36 ms (96.64%), Space: 17.1 MB (54.53%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,17 +3,17 @@ class Solution:
         return [num for row in matrix for num in row ]
 	
     def _binary_search(self, target, numbers, left, right):
-        if left < right:
+        if left <= right:
             middle = (left + right) // 2
             if target == numbers[middle]:
                 return True
             elif target > numbers[middle]:
                 return self._binary_search(target, numbers, middle+1, right)
             else :
-                return self._binary_search(target, numbers, left, middle)
+                return self._binary_search(target, numbers, left, middle-1)
         return False
 
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
         numbers = self._transfer_1d_list(matrix)
-        return self._binary_search(target, numbers, 0, len(numbers))
+        return self._binary_search(target, numbers, 0, len(numbers)-1)
