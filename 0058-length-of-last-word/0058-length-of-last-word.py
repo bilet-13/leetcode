47c5605ee8763554bcfc1f359b3985a5816c8491
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-08-09 18:21:16 +0800

    Time: 35 ms (54.71%), Space: 16.5 MB (52.01%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,16 +1,20 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         
+        if len(s) == 1:
+            return 1 if s != ' ' else 0
         
-        in_word = False
+        word_right_index = None
         length = 0
 
         for i in range( len(s)-1, -1, -1):
-            if s[i] != ' ':
-                in_word = True
-                length += 1
-                
-            elif in_word:
+            if not word_right_index and s[i] != ' ':
+                word_right_index = i
+
+            elif word_right_index and s[i] == ' ':
+                length = word_right_index - i
                 break
+            if i == 0:
+                length = len([char for char in s if char != ' '])
 
         return length
