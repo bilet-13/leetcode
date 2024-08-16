832a854c6bd1e2394af01f0be6d0877a893b4f4f
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-08-16 18:06:21 +0800

    Time: 60 ms (76.74%), Space: 21 MB (23.76%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,30 +6,22 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        result = []
+        self.maxPath(root, result)
+        
+        return result[0]
+
+    def maxPath(self, root, result):
         if root == None:
             return 0
-        elif not root.left and not root.right:
-            return root.val
+        left_gain = max(self.maxPath(root.left, result), 0)
+        right_gain = max(self.maxPath(root.right, result), 0)
 
-        left_path_sum = self.maxPathSumRoot(root.left)
-        right_path_sum = self.maxPathSumRoot(root.right)
-        root_max_path = root.val + (left_path_sum if left_path_sum > 0 else 0) + (right_path_sum if right_path_sum > 0 else 0)
-        
-        paths = [root_max_path]
-        if root.left:
-            left_sub_max = self.maxPathSum(root.left)
-            paths.append(left_sub_max)
-        if root.right:
-            right_sum_max = self.maxPathSum(root.right)
-            paths.append(right_sum_max)
-        return max(paths)
+        root_sum = root.val + left_gain + right_gain
 
-    def maxPathSumRoot(self, root):
-        if not root:
-            return 0
-        elif not root.left and not root.right:
-            return root.val
+        if result == []:
+            result.append(root_sum)
+        elif root_sum > result[0]:
+            result[0] = root_sum
 
-        return root.val + max(self.maxPathSumRoot(root.left), self.maxPathSumRoot(root.right), 0)
-
-        
+        return root.val + max(left_gain, right_gain) 
