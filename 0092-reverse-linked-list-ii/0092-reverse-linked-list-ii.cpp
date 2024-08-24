0c11ec8e2fafb8f3281a66f2b71c97357ffc4151
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-08-24 16:00:19 +0800

    Time: 5 ms (23.88%), Space: 10.6 MB (33.68%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,73 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        ListNode *node_ptr;
-        ListNode assist_head(7);
-        ListNode assist_tail(8);
-        ListNode *left_left_ptr;
-        ListNode *right_right_ptr;
-        ListNode *tail_ptr ;
-        ListNode *tmp_ptr;
-        stack<ListNode*> node_stack;
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* pre = dummy;
+        ListNode* next_node = nullptr;
+        ListNode*  cur = nullptr;
 
-        int index = 0;
-        bool tail_existed = true;
-
-        assist_head.next = head;
-
-        tail_ptr = &assist_head;
-        while(tail_ptr->next != NULL)
-            tail_ptr = tail_ptr->next;
-        tail_ptr->next = &assist_tail;
-
-        left_left_ptr = &assist_head;
-        right_right_ptr = &assist_head;
-
-        for (size_t i = 0; i < (left-1); i++)
-            left_left_ptr = left_left_ptr->next;
-
-        for (size_t i = 0; i < right+1 ; i++)
-            right_right_ptr = right_right_ptr->next;
-        
-        
-        node_ptr = &assist_head;
-        for (size_t i = 0; i < left; i++)
-            node_ptr = node_ptr->next;
-
-        for (size_t i = 0; i < (right - left + 1); i++)
-        {
-            node_stack.push(node_ptr);
-            node_ptr = node_ptr->next;
+        for(auto i = 0; i <left-1; i++){
+            pre = pre->next;
         }
-
-        tmp_ptr = node_stack.top();
-        node_stack.pop();
-        left_left_ptr->next = tmp_ptr;
-
-        while( !node_stack.empty()){
-            node_ptr = node_stack.top();
-            //cout<< node_ptr->val<<endl;
-            node_stack.pop();
-            tmp_ptr->next = node_ptr;
-            tmp_ptr = tmp_ptr->next;
+        cur = pre->next;
+        
+        for(auto i=left; i < right; i++){
+            next_node = cur->next;
+            cur->next = next_node->next;
+            next_node->next = pre->next;
+            pre->next = next_node;
         }
-        tmp_ptr->next = right_right_ptr;
-
-        tail_ptr = &assist_head;
-
-        while(  tail_ptr->next != &assist_tail)
-            tail_ptr = tail_ptr->next;
-        tail_ptr->next = NULL;
-        //cout<<tail_ptr->val<<endl;
-
-    //     node_ptr =assist_head.next;
-    //       while (node_ptr != NULL)
-    // {
-    //     cout<< node_ptr->val<<endl;
-    //     node_ptr = node_ptr->next;
-    // }
-        return assist_head.next;
-
+        return dummy->next;
     }
-        
 };
