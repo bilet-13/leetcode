6c971fb013816458232d6cf7cad279f99b3a04ee
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-08-27 16:04:30 +0800

    Time: 42 ms (76.7%), Space: 11.2 MB (64.68%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -3,15 +3,11 @@ public:
     int numSquares(int n) {
         vector<int> dp_table(n+1, INT_MAX);
         dp_table[0] = 0;
-        dp_table[1] = 1;
-
-        for(auto i = 1; i*i <= n; i++){
-            dp_table[i*i] = min(dp_table[i*i], 1);
-        }
-
-        for(auto i = 2; i <= n; i++){
-            for(auto j = 1; j <= i; j++ ){
-                dp_table[i] = min(dp_table[i], dp_table[i-j] + dp_table[j]);
+        
+        
+        for(auto i = 1; i <= n; i++){
+            for(auto j = 1; j*j <= i; j++ ){
+                dp_table[i] = min(dp_table[i], dp_table[i-j*j] + 1);
             }
         }
         return dp_table[n];
