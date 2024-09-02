8b5140fc65d1417bf9b46c85e1fcf315dfd57943
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-09-02 17:31:40 +0800

    Time: 35 ms (15.12%), Space: 94.9 MB (51.44%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int left = 0;
+        int right = m;
+
+        while (left <= right) {
+            int partitionX = (left + right) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            int minRightY = (partitionY == n) ? INT_MAX : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((m + n) % 2 == 0) {
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
+                } else {
+                    return max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                right = partitionX - 1;
+            } else {
+                left = partitionX + 1;
+            }
+        }
+
+        return 0.0;
+    }
+};
