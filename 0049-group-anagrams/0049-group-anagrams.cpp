7383c6077aae462f10983c731ce88df9b571b887
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-09-03 13:35:27 +0800

    Time: 23 ms (83.82%), Space: 22.9 MB (94.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,23 +4,15 @@ public:
         unordered_map<string, vector<string>> anagrams_group;
         vector<vector<string>> answer;
 
-        for (string str : strs){
+        for (const string& str : strs){
             string sorted_string = str;
             sort(sorted_string.begin(), sorted_string.end());
 
-            auto iter = anagrams_group.find(sorted_string);
-            if(iter != anagrams_group.end()){
-                iter->second.push_back(str);
-            }
-
-            else{
-                vector<string> tmp = {str};
-                anagrams_group[sorted_string] = tmp;
-            }
+            anagrams_group[sorted_string].push_back(str);
         }
 
-        for (auto anagram : anagrams_group){
-            answer.push_back(anagram.second);
+        for (auto& anagram : anagrams_group){
+            answer.push_back(move(anagram.second));
         }
 
         return answer;
