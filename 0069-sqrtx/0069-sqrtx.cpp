f6efee2e4f888b865c43c3ab990e06147a93d70f
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-09-13 18:25:05 +0800

    Time: 0 ms (100%), Space: 8.4 MB (19.95%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        if (x == 0){
+            return 0;
+        }
         long long int i = 0;
 
-        while(i*i < x){
-            i++;
+        int left = 1;
+        int right = x;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(x/mid == mid){
+                return mid;
+            }
+            else if (x/mid > mid){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
         }
-        return i*i == x ? i : i-1;
+        return right;
     }
 };
