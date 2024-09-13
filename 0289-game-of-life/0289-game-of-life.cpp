f8e054d8ca6b2cb7dab9d310c648438b232ed62d
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-09-13 17:11:42 +0800

    Time: 0 ms (100%), Space: 8.5 MB (56.1%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -4,10 +4,10 @@ public:
     void gameOfLife(vector<vector<int>>& board) {
         for(int i = 0 ; i < board.size() ;i++){
             for (int j = 0 ; j < board[0].size(); j++){
-                int neighbor = getLiveNeighbor(board, i , j);
-                if (neighbor == 3 && board[i][j] == 0)board[i][j] = 5;
-                else if(neighbor < 2 && board[i][j] == 1) board[i][j] = 2;
-                else if(neighbor > 3 && board[i][j] == 1) board[i][j] = 4;
+                int live_neighbor = getLiveNeighbor(board, i , j);
+                if (live_neighbor == 3 && board[i][j] == 0)board[i][j] = 5;
+                else if(live_neighbor < 2 && board[i][j] == 1) board[i][j] = 2;
+                else if(live_neighbor > 3 && board[i][j] == 1) board[i][j] = 4;
                 else if(board[i][j] == 1) board[i][j] = 3;
             }
         }
@@ -22,24 +22,25 @@ public:
     }
 
     bool isLive(vector<vector<int>>& board, int i, int j){
-        return (board[i][j] == 1 || board[i][j] == 2 || board[i][j] == 3 || board[i][j] == 4);
+        if(i >= 0 && i < board.size() && j >= 0 && j < board[0].size()){
+            return (board[i][j] == 1 || board[i][j] == 2 || board[i][j] == 3 || board[i][j] == 4);
+        }
+        return false;
     }
     int getLiveNeighbor(vector<vector<int>>& board, int i, int j){
         int neighbor = 0;
-        if (i - 1 >= 0){
-            if(j-1 >= 0 && isLive(board, i-1, j-1)) neighbor++;
-            if(isLive(board, i-1, j)) neighbor++;
-            if(j+1 < board[0].size() && isLive(board, i-1, j+1)) neighbor++;
-        }
+        
+        if(isLive(board, i-1, j-1)) neighbor++;
+        if(isLive(board, i-1, j)) neighbor++;
+        if(isLive(board, i-1, j+1)) neighbor++;
 
-        if(j-1 >= 0 && isLive(board, i, j-1)) neighbor++;
-        if(j+1 < board[0].size() && isLive(board, i, j+1)) neighbor++;
+        if(isLive(board, i, j-1)) neighbor++;
+        if(isLive(board, i, j+1)) neighbor++;
+        
+        if(isLive(board, i+1, j-1)) neighbor++;
+        if(isLive(board, i+1, j)) neighbor++;
+        if(isLive(board, i+1, j+1)) neighbor++;
 
-        if (i + 1 < board.size()){
-            if(j-1 >= 0 && isLive(board, i+1, j-1)) neighbor++;
-            if(isLive(board, i+1, j)) neighbor++;
-            if(j+1 < board[0].size() && isLive(board, i+1, j+1)) neighbor++;
-        }
         return neighbor;
     }
 };
