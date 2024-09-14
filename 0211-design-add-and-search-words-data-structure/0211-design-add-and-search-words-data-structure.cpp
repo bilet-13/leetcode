a53a530986605e665160fc7b459a6a49d1ea60dd
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-09-14 15:48:04 +0800

    Time: 1048 ms (17.57%), Space: 575.8 MB (35.32%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,66 @@
+struct Node{
+    unordered_map<char, Node*> child;
+    bool is_word;
+
+    Node(){
+        is_word = false;
+    }
+
+    
+};
+
+class WordDictionary {
+private:
+    Node* root;
+    
+    bool _dfs(Node* root, string word, int i){
+        if(i == word.size()){
+            return root->is_word;
+        }
+
+        if(i >= 0 && i < word.size()){
+            if(word[i] != '.'){
+                if(root->child.count(word[i]) != 0){
+                    return _dfs(root->child[word[i]], word, i+1);
+                }
+                return false;
+            }
+            else{
+                bool found = false;
+                for(auto node : root->child){
+                    found = found || _dfs(node.second, word, i+1);
+                }
+                return found;
+            }
+        }
+        return false;
+    }
+public:
+    WordDictionary() {
+        root = new Node();
+    }
+    
+    void addWord(string word) {
+        auto node = root;
+
+        for(char chr : word){
+            if(node->child.count(chr) == 0){
+                node->child[chr] = new Node(); 
+            }
+            node = node->child[chr];
+        }
+
+        node->is_word = true;
+    }
+    
+    bool search(string word) {
+        return _dfs(root, word, 0);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
