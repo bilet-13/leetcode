1524d20277042288c32f4fc94f76274152ab5945
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-09-21 00:28:06 +0800

    Time: 4 ms (21.91%), Space: 8.3 MB (43.78%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        if(x == 0.0 || x == 1.0 || n == 1){
+            return x;
+        }
+        if(n == 0){
+            return 1;
+        }
+
+        long long int power = n;
+        if(power < 0){
+            x = 1 / x;
+            power = -power; 
+        }
+
+        double odd_x = 1;
+
+        if(power%2 == 1){
+            power -= 1;
+            odd_x = x;
+        }
+
+        return odd_x * myPow(x*x, power/2);
     }
 };
