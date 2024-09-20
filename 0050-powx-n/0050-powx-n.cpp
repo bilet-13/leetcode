5dc19a6596e610264019a1a3b3687073b2eedcc7
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-09-21 00:28:30 +0800

    Time: 3 ms (38.91%), Space: 8.1 MB (97.33%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(x == 0.0 || x == 1.0 || n == 1){
+        if(x == 0.0 || n == 1){
             return x;
         }
         if(n == 0){
