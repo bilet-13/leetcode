b2b89c1c551ea065834966aed4f566ae8ee7d3e8
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2024-09-22 14:12:05 +0800

    Time: 286 ms (11.45%), Space: 138.4 MB (14.24%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    void getCombine(vector<vector<int>>& result, int start, int n, int k, vector<int> current){
+        if(current.size() == k){
+            result.push_back(current);
+            return;
+        }
+
+        for(int i = start; i <= n; i++){
+            current.push_back(i);
+            getCombine(result, i+1, n, k, current);
+            current.pop_back();
+        }
+        return;
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        getCombine(result, 1, n, k, {});
+
+        return result;
+    }
+};
