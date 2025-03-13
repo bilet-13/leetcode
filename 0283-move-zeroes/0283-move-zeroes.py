1733db557e5f11dbd8dce23ea5997083944518a2
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-03-13 22:11:41 +0800

    Time: 8 ms (31.57%), Space: 18.9 MB (49.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,18 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         left = 0
-        first_non_zero_num = 0      
-
-        while first_non_zero_num < len(nums) - 1 and left < len(nums) - 1:
-            while nums[first_non_zero_num] == 0 and first_non_zero_num < len(nums) - 1:
-                first_non_zero_num += 1
-
-            if nums[first_non_zero_num] == 0:
-                return
-
-            nums[first_non_zero_num], nums[left] = nums[left], nums[first_non_zero_num]
-            
-            left += 1
-            first_non_zero_num = left
+        
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
 
             
