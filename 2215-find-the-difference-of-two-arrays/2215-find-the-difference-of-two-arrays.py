fa1714a446e64705748a767c4e61d483e65466c7
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-03-15 22:16:39 +0800

    Time: 0 ms (100%), Space: 18.1 MB (67.8%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -3,8 +3,8 @@ class Solution:
         set1 = set(nums1)
         set2 = set(nums2)
 
-        unique_set1 = (set1 ^ set2) & set1
-        unique_set2 = (set1 ^ set2) & set2
+        unique_set1 = set1 - set2
+        unique_set2 = set2 - set1
 
         return [list(unique_set1), list(unique_set2)]
 
