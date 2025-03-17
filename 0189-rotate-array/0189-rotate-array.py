c84770f02080db2cb1f15d362f494059174c684d
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-03-17 22:20:23 +0800

    Time: 5 ms (52.03%), Space: 25.4 MB (92.02%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,18 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if k == 0 or len(nums) == 1:
-            return None
         k = k % len(nums)
-        self._reverse(nums, 0, len(nums)-1)
-        self._reverse(nums, 0, k-1)
-        self._reverse(nums, k, len(nums)-1)
 
+        if k == 0:
+            return
+        
+        queue = deque()
 
-    def _reverse(self, nums, start, end):
-        while start < end:
-            tmp_num = nums[start]
-            nums[start] = nums[end]
-            nums[end] = tmp_num
-            start += 1
-            end -= 1
+        for i in range(k, 0, -1):
+            queue.append(nums[len(nums)-i])
+
+        for i in range(0, len(nums)-k):
+            queue.append(nums[i])
+
+        for i in range(len(nums)):
+            nums[i] = queue.popleft()
+            
