3a68fccccb1e73b37371e35d97b68a83e31814ff
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-03-21 22:38:07 +0800

    Time: 7 ms (79.25%), Space: 18.9 MB (11.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,23 +1,23 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
-        
+        jump = 0
         pos = 0
-        num_jump = 0
-        farthest = 0 + nums[0]
-        next_pos = 0
+
+        while pos < len(nums)-1:
+            if pos + nums[pos] >= len(nums) - 1:
+                return jump + 1
+
+            start = pos
+            for i in range(1, nums[start]+1):
+                if nums[start + i] + start + i > nums[pos] + pos:
+                    pos = start + i
+            jump += 1
         
-        while pos < len(nums):
-            for i in range(1, nums[pos]+1):
-                neighbor = pos + i
-                if neighbor >= len(nums) - 1:
-                     return num_jump + 1
-                elif nums[neighbor] + neighbor > farthest:
-                    farthest = nums[neighbor] + neighbor
-                    next_pos = neighbor
-                    
-            num_jump += 1
-            pos = next_pos
+        return jump
+
+
+
+
+
+
         
-        return num_jump
