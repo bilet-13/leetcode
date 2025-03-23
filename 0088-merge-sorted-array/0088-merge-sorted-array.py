2b0ef4cfda45c4b861a035552a7101e7056f8570
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-03-23 17:16:21 +0800

    Time: 0 ms (100%), Space: 17.9 MB (26.43%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,26 +1,20 @@
-class Solution:
+class Solution: 
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1_end = m - 1
-        nums2_end = n - 1
+        write_index = m + n - 1
+        index1 = m - 1
+        index2 = n - 1
 
-        for index in range(m + n - 1, -1, -1):
-            if nums1_end >= 0 and nums2_end >= 0:
-                if nums1[nums1_end] > nums2[nums2_end]:
-                    nums1[index] = nums1[nums1_end]
-                    nums1_end -= 1
-                else:
-                    nums1[index] = nums2[nums2_end]
-                    nums2_end -= 1
-
-            elif nums1_end >= 0:
-                nums1[index] = nums1[nums1_end]
-                nums1_end -= 1
+        while index2 >= 0:
+            if index1 >= 0 and nums1[index1] > nums2[index2]:
+                nums1[write_index] = nums1[index1]
+                index1 -= 1
             else:
-                nums1[index] = nums2[nums2_end]
-                nums2_end -= 1
+                nums1[write_index] = nums2[index2]
+                index2 -= 1
+            write_index -= 1
 
 
 
