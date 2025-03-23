b011850522bb1a47a7360bab3a0503100d12f4c0
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-03-23 16:48:32 +0800

    Time: 0 ms (100%), Space: 17.9 MB (26.43%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,31 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums3 = [0] * (m+n)
-        index1 = 0
-        index2 = 0
-        index3 = 0
+        nums1_end = m - 1
+        nums2_end = n - 1
 
-        while index1 < m and index2 < n:
-            if(nums1[index1] <= nums2[index2]):
-                nums3[index3] = nums1[index1]
-                index1 += 1
+        for index in range(m + n - 1, -1, -1):
+            if nums1_end >= 0 and nums2_end >= 0:
+                if nums1[nums1_end] > nums2[nums2_end]:
+                    nums1[index] = nums1[nums1_end]
+                    nums1_end -= 1
+                else:
+                    nums1[index] = nums2[nums2_end]
+                    nums2_end -= 1
+
+            elif nums1_end >= 0:
+                nums1[index] = nums1[nums1_end]
+                nums1_end -= 1
             else:
-                nums3[index3] = nums2[index2]
-                index2 += 1
-            index3 += 1
+                nums1[index] = nums2[nums2_end]
+                nums2_end -= 1
 
-        if index1 != m:
-            while index1 < m: 
-                nums3[index3] = nums1[index1]
-                index1 += 1
-                index3 += 1
 
-        elif index2 != n:
-            while index2 < n: 
-                nums3[index3] = nums2[index2]
-                index2 += 1
-                index3 += 1
-        
-        for i in range(len(nums3)):
-            nums1[i] = nums3[i]
+
