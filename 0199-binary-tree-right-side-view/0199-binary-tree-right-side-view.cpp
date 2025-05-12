e5d53d7e6116d931f12f5c5acb20517a53868c11
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-05-12 23:14:47 +0800

    Time: 0 ms (100%), Space: 15 MB (62.99%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,44 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        queue<TreeNode*> nodes;
+        TreeNode *rightestNode;
 
-        vector<int> answer_nodes;
-        queue< pair<TreeNode*, int> > nodeLevelQueue;
-        vector< pair<TreeNode*, int> > nodeLevelVec;
-
-        if(root == NULL){
-            return answer_nodes;
+        if (root != nullptr) {
+            nodes.push(root);
         }
-        nodeLevelQueue.push(make_pair(root, 0));
 
-        while(!nodeLevelQueue.empty()){
+        while (!nodes.empty()) {
+            auto size = nodes.size();
 
-            pair<TreeNode*, int> tmp_nodeLevel;
+            for (int i = 0; i < size; i++) {
+                auto node = nodes.front();
+                nodes.pop();
 
-            tmp_nodeLevel = nodeLevelQueue.front();
-            nodeLevelQueue.pop();
-
-            nodeLevelVec.push_back(tmp_nodeLevel);
-
-            if(tmp_nodeLevel.first != NULL){
-                nodeLevelQueue.push(make_pair(tmp_nodeLevel.first->right, 
-                                     tmp_nodeLevel.second + 1));
-                nodeLevelQueue.push(make_pair(tmp_nodeLevel.first->left, 
-                                     tmp_nodeLevel.second + 1));
+                if (node->left != nullptr) {
+                    nodes.push(node->left);
+                }
+                if(node->right != nullptr) {
+                    nodes.push(node->right);
+                }
+                rightestNode = node;
             }
+            result.push_back(rightestNode->val);
         }
-
-        int level = -1;
-
-        for(vector< pair<TreeNode*, int> >::iterator it = nodeLevelVec.begin(); it != nodeLevelVec.end(); it++){
-            if(it->second > level && it->first != NULL){
-                answer_nodes.push_back(it->first->val);
-                level++;
-            }
-        }
-        return answer_nodes;
+        return result;
     }
 };
