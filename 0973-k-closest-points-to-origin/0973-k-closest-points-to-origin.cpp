e9a31440195fe3c63a5f4f8c32eb3bcd2a904017
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-05-21 23:00:39 +0800

    Time: 99 ms (29.51%), Space: 81.3 MB (28.81%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,25 +1,25 @@
 class Solution {
 private:
-    static double calculateDistance(const vector<int> &point) {
+    static double calculateDistanceSquare(const vector<int> &point) {
         return point[0] * point[0] + point[1] * point[1];
     }
 
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         auto cmp = [](const vector<int> &a, const vector<int> &b) {
-            return calculateDistance(a) < calculateDistance(b);
+            return calculateDistanceSquare(a) < calculateDistanceSquare(b);
         };
         // size k min heap
         priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> maxHeap(cmp);
 
         // for num if num <= heap.top do nothing else heap.pop() heap.push(num)
         for (vector<int> point : points) {
-            int distance = calculateDistance(point);
+            int distance = calculateDistanceSquare(point);
 
             if (maxHeap.size() < k) {
                 maxHeap.push(point);
             } else {
-                if (calculateDistance(maxHeap.top()) > distance) {
+                if (calculateDistanceSquare(maxHeap.top()) > distance) {
                     maxHeap.pop();
                     maxHeap.push(point);
                 }
