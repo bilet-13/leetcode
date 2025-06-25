de1fc39602ce996dc9b69294b244f5a0bc242ac5
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-06-25 23:45:25 +0800

    Time: 4 ms (68.43%), Space: 19.2 MB (74.69%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,47 +1,39 @@
-class Node {
-    public:
-        unordered_set<int> inNodes;
-        unordered_set<int> outNodes;
-};
-
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        if (prerequisites.size() == 0) {
-            return true;
+        vector<int> inDeg(numCourses, 0);
+        vector<vector<int>> graph(numCourses);
+        queue<int> zeroInDegree;
+
+        for (auto& preReq: prerequisites) {
+            int course = preReq[0];
+            int preCourse = preReq[1];
+
+            inDeg[course]++;
+            graph[preCourse].push_back(course);
         }
 
-        vector<Node> nodes(numCourses, Node());
-        unordered_set<int> takenCourse;
-
-        for (auto &prequisite: prerequisites) {
-            int course = prequisite[0];
-            int preCourse = prequisite[1];
-
-            nodes[course].inNodes.insert(preCourse);
-            nodes[preCourse].outNodes.insert(course);
+        for (int i = 0; i < numCourses; ++i) {
+            if (inDeg[i] == 0) {
+                zeroInDegree.push(i);
+            }
         }
         
-        while (takenCourse.size() < numCourses) {
-            int preCourse = -1;
+        int courseCount = 0;
+        while (!zeroInDegree.empty()) {
+            auto curCourse = zeroInDegree.front();
+            zeroInDegree.pop();
 
-            for (int i = 0; i < numCourses; ++i) {
-                if (nodes[i].inNodes.size() == 0 && takenCourse.find(i) == takenCourse.end()) {
-                    preCourse = i;
-                    takenCourse.insert(i);
-                    break;
+            courseCount++;
+
+            for (auto nextCourse: graph[curCourse]) {
+                inDeg[nextCourse]--;
+                if (inDeg[nextCourse] == 0) {
+                    zeroInDegree.push(nextCourse);
                 }
             }
-
-            if (preCourse == -1) {
-                return false;
-            }
-
-            for (int course: nodes[preCourse].outNodes) {
-                nodes[course].inNodes.erase(preCourse);
-            }
         }
-        
-        return true;
+
+        return courseCount == numCourses;
     }
 };
