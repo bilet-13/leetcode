325c599bda564d66900a43287e8417f2cb38f2d8
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-06-27 23:16:37 +0800

    Time: 0 ms (100%), Space: 17.9 MB (89.84%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        //kahn's algo
+        // find topological sort order
+        vector<int> order;
+        vector<vector<int>> graph(numCourses); // adjancy list
+        vector<int> inDegree(numCourses, 0);
+        queue<int> q;
+
+        for (const auto & preReq: prerequisites) {
+            int course = preReq[0];
+            int preCourse = preReq[1];
+
+            graph[preCourse].push_back(course);
+            inDegree[course]++;
+        }
+
+        for (int i = 0; i < numCourses; ++i) {
+            if (inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while (!q.empty()) {
+            auto course = q.front();
+            q.pop();
+
+            order.push_back(course);
+
+            for (const auto& nextCourse: graph[course]) {
+                if (--inDegree[nextCourse] == 0) {
+                    q.push(nextCourse);
+                }
+            }
+        }
+        
+        return (order.size() == numCourses) ? order : vector<int>();
+    }
+};
