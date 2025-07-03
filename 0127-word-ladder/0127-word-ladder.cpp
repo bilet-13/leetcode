27a0b3c50a762ce51ed866e14dbe76fbbd364f57
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-07-03 22:34:12 +0800

    Time: 139 ms (41.66%), Space: 24.5 MB (20.93%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -6,49 +6,40 @@ public:
         // word a can transofro to word b => a -> b
         // build the graph with 0: beginword 1 - n word in wordList
         int n = wordList.size();
-        vector<vector<int>> graph(n + 1);
-        vector<bool> visited(n + 1, false);
-        queue<pair<int, int>> q;
+        unordered_set<string> words(wordList.begin(), wordList.end());
+        unordered_set<string> visited;
+        queue<pair<string, int>> q;
 
-        // time complexity o(N^2L) // L = word length
-        for (int i = 0; i <= n; ++i) {
-            auto& cur = (i == 0) ? beginWord : wordList[i - 1];
-
-            for (int k = 0; k < n; ++k) {
-                auto& word = wordList[k];
-                int diffNum = 0;
-
-                for (int j = 0; j < word.size(); ++j) {
-                    diffNum += cur[j] != word[j] ? 1 : 0;
-                }
-                if (diffNum == 1) {
-                    graph[i].push_back(k + 1);
-                }
-            }
-        }
-
-        q.emplace(0, 1);
+        q.emplace(beginWord, 1);
         while (!q.empty()) {
-            auto [node, step] = q.front();
+            auto [word, step] = q.front();
             q.pop();
 
-            if (visited[node]) {
+            if (visited.count(word)) {
                 continue;
             }
-            visited[node] = true;
+            visited.insert(word);
 
-            auto& cur = (node == 0) ? beginWord : wordList[node - 1];
-            if (cur == endWord) {
+            if (endWord == word) {
                 return step;
             }
 
-            for (auto &neighbor: graph[node]) {
-                if (!visited[neighbor]) {
-                    q.emplace(neighbor, step + 1);
+            for (int i = 0; i < word.size(); ++i) {
+                auto origin = word[i];
+
+                for (char chr = 'a'; chr <= 'z'; chr++) {
+                    if (origin == chr) {
+                        continue;
+                    }
+                    word[i] = chr;
+                    if (!visited.count(word) && words.count(word)) {
+                        q.emplace(word, step + 1);
+                    }
                 }
+
+                word[i] = origin;
             }
         }
-        // do bfs return the shorstest path between beginWord and endWord else retun 0
         return 0;
     }
 };
