1bbeda9a842693d8eaf3dc1764d60b0fb599516d
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-07-23 23:23:40 +0800

    Time: 15 ms (93.63%), Space: 17.9 MB (74.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-
-        vector<int> dp(amount+1, -1);
-
+        // dp 
+        // coins: 1, 5, 10 dp[i] = min(dp[i - 1], dp[i - 5], dp[i - 10]) + 1
+        vector<int> dp(amount + 1, INT_MAX); // dp[i]: the fewest number of coins to make up i
         dp[0] = 0;
-
-        for(const auto& num : coins){
-            if(num <= amount){
-                dp[num] = 1;
+        
+        for (const auto& coin : coins) {
+            if (coin <= amount) {
+                dp[coin] = 1;
             }
         }
 
-        for(int i = 1 ; i <= amount; i++){
-            for(const auto& num : coins){
-                if(num <= i &&  dp[i - num] != -1){
-                    dp[i] = dp[i] == -1 ? dp[i - num] + dp[num] : min(dp[i], dp[i - num] + dp[num]);
+        for (int i = 1; i <= amount; ++i) {
+            int numCoins = INT_MAX;
+            
+            for (const auto& coin : coins) {
+                if (i - coin >= 0 && i - coin <= amount) {
+                    numCoins = min(numCoins, dp[i - coin]);
                 }
             }
+            if (numCoins == INT_MAX) {
+                continue;
+            }
+
+            dp[i] = min(dp[i], numCoins + 1);
         }
-        return dp[amount];
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
