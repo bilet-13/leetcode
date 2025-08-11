4c67a269ec6393aaa6da0a35abc7801d33e67bbc
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-08-11 20:44:33 +0800

    Time: 55 ms (14.77%), Space: 26.4 MB (13.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1,INT_MAX));
-
-        for(int i = 0; i <= word1.size(); i++){
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0)); 
+        // dp[i][j]: the minimum number of ops to let the string consiti of 
+        // first i char of word1 equal to the string consist of the first j char of word2
+        for (int i = 0; i <= n; ++i) {
             dp[i][0] = i;
         }
-        
-        for(int j = 0; j <= word2.size(); j++){
+
+        for (int j = 0; j <= m; ++j) {
             dp[0][j] = j;
         }
 
-        for(int i = 1; i <= word1.size(); i++){
-            for(int j = 1; j <= word2.size(); j++){
-                if(word1[i-1] == word2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-
-                else{
-                    int del_cost = dp[i-1][j] + 1;
-                    int insert_cost = dp[i][j-1] + 1;
-                    int replace_cost = dp[i-1][j-1] + 1;
-                    
-                    dp[i][j] = (del_cost < insert_cost && del_cost < replace_cost) ? del_cost : (insert_cost < replace_cost) ? insert_cost : replace_cost; 
+        for (int i = 1; i <=n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    vector<int> candidates = {dp[i - 1][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j - 1] + 1};
+                    // candidates:1 replace word1[i -1] to word2[j - 1 ], 2 deleta a char then do operation that make first i - 1 char to first j char, 3 after an operation that make first i char to j - char and then insert a char
+                    dp[i][j] = *min_element(candidates.begin(), candidates.end());
                 }
             }
         }
 
-        return dp[word1.size()][word2.size()];
-
+        return dp[n][m];
     }
 };
