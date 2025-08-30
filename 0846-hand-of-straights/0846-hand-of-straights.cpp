6aa343f491eabbc350aa986d54df929f116795ef
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-08-30 21:10:02 +0800

    Time: 25 ms (86.5%), Space: 30.7 MB (90.03%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        unordered_map<int, int> frequencies;
+
+        if ((n % groupSize) != 0) {
+            return false;
+        }
+
+        sort(hand.begin(), hand.end()); // nlogn
+
+        for (const auto& num: hand) {
+            frequencies[num] += 1;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (frequencies.count(hand[i]) && frequencies[hand[i]] != 0) {
+                int num = hand[i];
+                frequencies[num]--;
+
+                for (int j = 1; j < groupSize; ++j) {
+                    num++;
+                    if (!frequencies.count(num) || frequencies[num] == 0) {
+                        return false;
+                    }
+                    frequencies[num]--;
+                    
+                }
+            }
+        } // o(n)
+
+        return true;// o(nlogn)
+    }
+};
