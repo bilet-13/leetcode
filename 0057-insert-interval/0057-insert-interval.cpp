97b2579705b434c457c3db8e9b2f52236b05983b
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-09-16 17:18:38 -0700

    Time: 6 ms (9.53%), Space: 24.2 MB (5.36%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,52 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int pos = findInsertPos(intervals, newInterval);
-        intervals.insert(intervals.begin()+pos, newInterval);
+        // step 1: insert new interval to interval and keep ascending
+        // time complexity O(n + 1) n: size of intervals
+        vector<vector<int>> allIntervals;
+        vector<vector<int>> mergedIntervals;
+        bool isNewIntervalInAllIntervals = false;
 
-        mergeIntervals(intervals, pos);
-        return intervals;
-    }
-
-    int findInsertPos(vector<vector<int>>& intervals, vector<int>& newInterval){
-        int left = 0;
-        int right = intervals.size() - 1;
-
-        while(left <= right){
-            int mid = left + (right-left) / 2;
-
-            if(intervals[mid][0] == newInterval[0]){
-                return mid;
+        for (int i = 0; i < intervals.size(); ++i) {
+            if (intervals[i][0] >= newInterval[0]) {
+                allIntervals.push_back(newInterval);
+                isNewIntervalInAllIntervals = true;
             }
-            else if(intervals[mid][0] > newInterval[0]){
-                right = mid - 1;
-            }
-            else{
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
-
-    void mergeIntervals(vector<vector<int>>& intervals, int pos){
-        int left = pos;
-        int right = pos;
-
-        int start = intervals[pos][0];
-        int end = intervals[pos][1];
-
-        while(left-1 >= 0 && intervals[left-1][1] >= start){
-            left--;
-            start = min(intervals[left][0], start);
-            end = max(intervals[left][1], end);
+            allIntervals.push_back(intervals[i]);
         }
 
-        while(right+1 < intervals.size() && intervals[right+1][0] <= end){
-            right++;
-            end = max(intervals[right][1], end);
+        if (!isNewIntervalInAllIntervals) {
+            allIntervals.push_back(newInterval);
         }
+        mergedIntervals.push_back(allIntervals[0]);
+        
+        // step 2: turn the intervals to non overlaping by merging
+        // time complexity O(n + 1) n + 1(new interval) n: size of intervals
+        for (int i = 0; i < allIntervals.size(); ++i) {
+            int last = mergedIntervals.size() - 1; 
+            if (mergedIntervals[last][1] < allIntervals[i][0]) {
+                mergedIntervals.push_back(allIntervals[i]);
+                continue;
+            }
 
-        auto iter = intervals.erase(intervals.begin()+left, intervals.begin()+right+1);
-        intervals.insert(iter, {start, end});
+            mergedIntervals[last][1] = max(mergedIntervals[last][1], allIntervals[i][1]);
+        }
+       
+        return mergedIntervals;// time complexity O(n + 1) = O(n) n: size of the vector intervals
     }
 };
