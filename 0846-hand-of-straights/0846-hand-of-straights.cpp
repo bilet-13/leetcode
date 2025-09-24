8ff8e73446791fb59d2fc235043db184c5c28f3f
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-09-23 22:09:56 -0700

    Time: 39 ms (56.2%), Space: 31 MB (85.62%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -25,7 +25,6 @@ public:
                         return false;
                     }
                     frequencies[num]--;
-                    
                 }
             }
         } // o(n)
