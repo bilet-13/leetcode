22a28915f155ce39a5aa713c5612e6b4c1792c9a
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-09-25 12:36:19 -0700

    Time: 0 ms (100%), Space: 10.3 MB (35.39%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        reverse(matrix.begin(), matrix.end());
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < i; j++){
+        int n = matrix.size();
+
+        // flip matrix by its diagonal
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
                 swap(matrix[i][j], matrix[j][i]);
+            }   
+        }
+
+        // flip matrix by straight line
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < int(n / 2); ++j) {
+                swap(matrix[i][j], matrix[i][n - j - 1]);
             }
         }
+        return;
     }
 };
