b803a7fe4256953f4f54e55ee335ab3fde30b116
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-09-28 16:34:14 -0700

    Time: 0 ms (100%), Space: 7.8 MB (72.81%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,34 +1,35 @@
 class Solution {
-    int getSquareSum(int n){
-        auto sum = 0;
-        int digit;
-
-        while (n) {
-            digit = n % 10;
-            n /= 10;
-            sum += pow(digit, 2);
-        }
-        return sum;
-    }
-
 public:
     bool isHappy(int n) {
-        auto slow = n;
-        auto fast = n;
-
-        while(true){
-            slow = getSquareSum(slow);
-            fast = getSquareSum(fast);
-            fast = getSquareSum(fast);
-
-            if (fast == 1){
-                return true;
+        // two pointer
+        // slow , fast pointer
+        // slow do 1 step each time and fast do 2 steps each time
+        // if slow == fast cycle
+        // else if slow or fast == 1 return true
+        auto getSquareSum = [](int n) -> int {
+            int result = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                n = n / 10;
+                result += pow(digit, 2);
             }
+            return result;
+        };
 
-            if(fast == slow){
+        int slow = n;
+        int fast = n;
+
+        while (slow != 1 && fast != 1) {
+            slow = getSquareSum(slow);
+
+            fast = getSquareSum(fast);
+            fast = getSquareSum(fast);
+
+            if (slow != 1 && slow == fast) {
                 return false;
             }
         }
 
+        return true;
     }
 };
