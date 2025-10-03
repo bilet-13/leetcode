cdf5f46a7d318fde1ec688fc1f44b57e6fb6ebf7
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-10-03 15:46:01 -0700

    Time: 0 ms (100%), Space: 8.6 MB (44.47%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(x == 0.0 || n == 1){
-            return x;
-        }
-        if(n == 0){
-            return 1;
-        }
+       long long int k = n;
+       if (k < 0) {
+        k = -k;
+       }
+       double result = pow(x, k);
 
-        long long int power = n;
-        if(power < 0){
-            x = 1 / x;
-            power = -power; 
-        }
+       return n >= 0 ? result : 1 / result;
+    }
 
-        double odd_x = 1;
-
-        if(power%2 == 1){
-            power -= 1;
-            odd_x = x;
-        }
-
-        return odd_x * myPow(x*x, power/2);
+    double pow(double x, long long int n) {
+        if (n == 0) {
+        return 1;
+       } else if (n == 1) {
+        return x;
+       } else if (n % 2 == 0) {
+        double result = pow(x, n / 2);
+        return result * result;
+       } else {
+        double result = pow(x, (n - 1) / 2); 
+        return x * result * result;
+       }
     }
 };
