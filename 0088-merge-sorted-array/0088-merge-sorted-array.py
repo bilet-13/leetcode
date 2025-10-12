7300c4640d0a7aad24f864d14b58fe364178f24b
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-10-11 22:42:15 -0700

    Time: 0 ms (100%), Space: 18 MB (26.18%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -8,7 +8,7 @@ class Solution:
         nums2_idx = n - 1
 
         while merge_idx >= 0:
-            if (nums1_idx >= 0 and nums1[nums1_idx] >= nums2[nums2_idx] and nums2_idx >= 0) or nums2_idx < 0 :
+            if (nums1_idx >= 0 and nums1[nums1_idx] >= nums2[nums2_idx] and nums2_idx >= 0) or  nums2_idx < 0:
                 nums1[merge_idx] = nums1[nums1_idx]
                 nums1_idx -= 1
             else:
