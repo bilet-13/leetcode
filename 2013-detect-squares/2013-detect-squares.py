fff8c38b49ef7ec3671e660e53d639a92934b119
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-10-14 22:03:07 -0700

    Time: 27 ms (98.32%), Space: 19.6 MB (28.57%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -2,21 +2,13 @@ class DetectSquares:
 
     def __init__(self):
         # internal data stucture {x: {y: time}}
-        self.pt = defaultdict(int)
-        self.column = {}
+        self.column = defaultdict(lambda: defaultdict(int))
 
     def add(self, point: List[int]) -> None:
         # add the new point to correspond list
         x, y = point
-    
-        self.pt[(x, y)] += 1
-
-        if x not in self.column:
-            self.column[x] = defaultdict(int)
-        
         self.column[x][y] += 1
         
-
     def count(self, point: List[int]) -> int:
         # to check that if the input point on left lower, left higher, right lower, right upper
         # can form a square or not. time complexity 
@@ -30,17 +22,17 @@ class DetectSquares:
         result = 0
 
         same_col = self.column[x]
-        point_count = self.column[x][y]
 
         for c_y, c_count in same_col.items():
             length = abs(c_y - y)
             if c_y == y:
                 continue
 
-            result +=  c_count * self.pt[(x + length, y)] * self.pt[(x + length, c_y)]
-            result +=  c_count * self.pt[(x - length, y)] * self.pt[(x - length, c_y)]
+            result +=  c_count * self.column[x + length][y] * self.column[x + length][c_y]
+            result +=  c_count * self.column[x - length][y] * self.column[x - length][c_y]
 
         return result
+
         
 
 
