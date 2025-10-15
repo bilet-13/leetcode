ec5c63e976e692a70bbc38d3354d12ed38c85fe4
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-10-15 10:50:42 -0700

    Time: 86 ms (48.87%), Space: 20.5 MB (18.13%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,31 +1,21 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        [123334]
-        [123343]
-        duplicated_num = nums[0]
-        count = 1
-        insert_pos = 1
+        # double pointer, one for the element, one for the element that are at most twice
+        # one variable duplicate_num the duplicate_num so far
+
+        unique_idx = 1
+        duplicate_num = 1
 
         for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
-                if count < 2:
-                    nums[insert_pos] = nums[i]
-                    count += 1
-                    insert_pos += 1
-                else:
-                    count += 1
+            if nums[i] != nums[i - 1]:
+                nums[unique_idx] = nums[i]
+                unique_idx += 1
+                duplicate_num = 1
+            
+            elif duplicate_num < 2:
+                nums[unique_idx] = nums[i]
+                unique_idx += 1
+                duplicate_num += 1
 
-            else:
-                count = 1
-                duplicated_num = nums[i]
-                nums[insert_pos] = nums[i]
-                insert_pos += 1
+        return unique_idx # 
         
-        
-        return insert_pos
-
-    def push_end(self, nums, index, end):
-        tmp_num = nums[index]
-        for i in range(index, end-1):
-            nums[i] = nums[i+1]
-        nums[end-1] = tmp_num
