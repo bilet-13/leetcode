f255f97d7e2ef2fe686f772292efdbbae3335552
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-10-16 22:08:37 -0700

    Time: 2 ms (18.64%), Space: 17.9 MB (10.19%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -6,5 +6,5 @@ class Solution:
             last_bit = n & 1
             n = n >> 1
             
-            num_one_bit += 1 if last_bit == 1 else 0
+            num_one_bit += last_bit 
         return num_one_bit 
