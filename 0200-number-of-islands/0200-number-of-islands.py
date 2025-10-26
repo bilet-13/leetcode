59d840e65aa6f7135fb5f45ae9ccc8c7707d0ad9
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-10-25 22:45:26 -0700

    Time: 241 ms (68.11%), Space: 19.9 MB (95.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,34 @@
 class Solution:
-    def BFS(self, grid, root):
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        queue = [root]
-
-        while queue:
-            x, y = queue.pop(0)
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == '1':
-                    grid[nx][ny] = '0'
-                    queue.append((nx, ny))
-
-
     def numIslands(self, grid: List[List[str]]) -> int:
+        # travsal every unvisited cell do BFS and 
+        # mark the vistied cell to 2
+        # use var to count the num of island
+        count = 0
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         m = len(grid)
         n = len(grid[0])
 
-        islands = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    islands += 1
+        def BFS(grid, i, j):
+            queue = deque()
+            queue.append((i, j))
+            grid[i][j] = '2'
 
-                    self.BFS(grid,(i, j))
+            while queue:
+                x, y = queue.popleft()
 
+                for dx, dy in dirs:
+                    nx = x + dx
+                    ny = y + dy
 
-        return islands
+                    if m > nx >= 0 and n > ny >= 0 and grid[nx][ny] == '1':
+                        grid[nx][ny] = '2'
+                        queue.append((nx, ny)) 
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    count += 1
+                    BFS(grid, i, j)
+
+        return count
+        
