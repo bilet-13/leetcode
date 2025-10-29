0dc18c9252c7651a715768262f1922b34e01d6ff
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-10-29 10:53:35 -0700

    Time: 5 ms (33.91%), Space: 18.1 MB (5.55%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,6 +2,7 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
+        # time complexity o(nm), calcuate roten and vvisited and fresh num  + BFS = o(V+E) = o(nm)
 
         fresh_num = sum([1 for i in range(m) for j in range(n) if grid[i][j] == 1])
 
@@ -14,8 +15,6 @@ class Solution:
         while queue:
             x, y, time = queue.popleft()
 
-            if grid[x][y] == 1:
-                fresh_num -= 1
             minimum_time = max(minimum_time, time)
             
             for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
@@ -23,6 +22,7 @@ class Solution:
                 ny = y + dy
 
                 if m > nx >= 0 and n > ny >= 0 and grid[nx][ny] == 1 and not visited[nx][ny]:
+                    fresh_num -= 1
                     visited[nx][ny] = True
                     queue.append((nx, ny, time + 1))
         
