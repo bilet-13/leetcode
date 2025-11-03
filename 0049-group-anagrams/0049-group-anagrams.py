c53372278d616f028e2c5c55e34118cd605ba202
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-11-03 10:57:06 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = {} # key: tuple() 26 element represesnt the number of each char g val: array of strs
+        # for each strs check it is anagram with key
+        # if yes add it to array, if not create new group
+        # time complexity o(n * check anagramn) = o(n * len of curr str)
+
+        for word in strs:
+            frequency = [0 for _ in range(26)]
+
+            for char in word:
+                frequency[ord(char) - ord('a')] += 1
+            key = tuple(frequency)
+
+            if key not in group:
+                group[key] = [word]
+            else:
+                group[key].append(word)
+
+        return [group[key] for key in group]
+        
