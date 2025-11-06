19864d16166a6a6029e20e8ee00e715a03cc2f37
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-11-05 17:42:44 -0800

    Time: 5 ms (57.17%), Space: 19.2 MB (61.7%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,36 +1,47 @@
 class Solution:
-    def build_course_graph(self, numCourses, prerequisites):
-        graph = {i:{'in':[], 'out': []} for i in range(numCourses)}
-        
-        for course, pre_course in prerequisites:
-            graph[course]['in'].append(pre_course)
-            graph[pre_course]['out'].append(course)
-            
-        return graph
-
-    def check_cycle(self, graph):
-
-        queue = deque([node for node in graph if not graph[node]['in']])
-        visited = set(queue)
-
-        if not queue:
-            return True
-
-        while queue:
-            node = queue.pop()
-            for neighbor in graph[node]['out']:
-                graph[neighbor]['in'].remove(node)
-                if not graph[neighbor]['in']:
-                    queue.append(neighbor)
-                    visited.add(neighbor)
-
-            if not queue and len(visited) != len(graph.keys()):
-                return True
-                 
-        return False
-
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if not prerequisites:
+           # topological sort or no cycle
+        # check the graph has cycle or not
+        # node: label of couse . edge b -> a if b is prerequisite course of a
+        # use list[numCourse] to check visite or not 
+        # for not state cell, do dfs to check is there has cycle
+        # time complexity o(V + E) = o(n ^ 2)
+        # edge case if numCOurse = 1 return true if len of prerequisites == 0 return true
+        if numCourses <= 1 or len(prerequisites) < 1:
             return True
-        graph = self.build_course_graph(numCourses, prerequisites)
-        return not self.check_cycle(graph)
+        state = [0 for _ in range(numCourses)] # 0 unstate 1 visiting 2 vistied
+        edges = [[] for _ in range(numCourses)]
+        for request in prerequisites:
+            edges[request[1]].append(request[0])
+
+        def check_cycle(node):
+            stack = [(node, 0)]
+
+            while stack:
+                node, phase = stack.pop()
+
+                if phase == 0:
+                    if state[node] == 2:
+                        continue
+                    elif state[node] == 1:
+                        return True
+                    
+                    state[node] = 1
+                    stack.append((node, 1))
+                    for course in edges[node]:
+                        if state[course] == 1:
+                            return True
+                        elif state[course] == 0:
+                            stack.append((course, 0))
+                else:
+                    state[node] = 2
+    
+            return False
+
+        for i in range(numCourses):
+            if state[i] == 0:
+                if check_cycle(i):
+                    return False
+        return True
+
+        
