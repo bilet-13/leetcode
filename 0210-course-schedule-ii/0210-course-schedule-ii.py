bcafdf13ab32ac5e69ea940296bb2a021fc516f4
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-11-08 10:59:52 -0800

    Time: 5 ms (36.21%), Space: 19.3 MB (47.97%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,23 +1,32 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        in_degrees = [0 for _ in range(numCourses)]
+        visited = [0 for _ in range(numCourses)]
         edges = [[] for _ in range(numCourses)]
+        has_cycle = False
         order = []
 
         for pre in prerequisites:
             edges[pre[1]].append(pre[0])
-            in_degrees[pre[0]] += 1
-        
-        queue = deque(course for course in range(numCourses) if in_degrees[course] == 0)
 
-        while queue:
-            cur = queue.popleft()
-            order.append(cur)
+        def DFS(cur):
+            nonlocal has_cycle
+            if visited[cur] == 2:
+                return
+            elif visited[cur] == 1:
+                has_cycle = True
+                return
 
+            visited[cur] = 1
             for nxt in edges[cur]:
-                in_degrees[nxt] -= 1
-                if in_degrees[nxt] == 0:
-                    queue.append(nxt)  
+                DFS(nxt)
+
+            visited[cur] = 2
+            order.append(cur)
+            return
+
+        for course in range(numCourses):
+            if visited[course] == 0:
+                DFS(course)
         
-        return order if len(order) == numCourses else []
+        return [] if has_cycle else order[::-1]
         
