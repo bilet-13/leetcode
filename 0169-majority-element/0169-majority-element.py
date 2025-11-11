694158b1c170e999ddd01b1693a23d16e66d21b7
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-11-11 10:44:52 -0800

    Time: 8 ms (37.56%), Space: 19.3 MB (86.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        num_and_count = [nums[0], 1] # number, count
+        #tupel: read only
+        candidate, count = nums[0], 1
 
         for i in range(1, len(nums)):
-            if nums[i] != num_and_count[0]:
-                num_and_count[1] -= 1
+            if nums[i] != candidate:
+                count -= 1
                 
-                if num_and_count[1] == 0:
-                    num_and_count = [nums[i], 1]
+                if count == 0:
+                    candidate, count = nums[i], 1
             else:
-                num_and_count[1] += 1
+                count += 1
 
-        return num_and_count[0]
+        return candidate
 
         
