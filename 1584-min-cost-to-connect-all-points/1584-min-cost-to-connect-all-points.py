be4b33443f2e97581b1fc3cce6f8bcb2723bf494
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-11-23 11:07:45 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+         pq = []
+        n = len(points)
+        min_cost = 0
+
+        def add_edges(x, y):
+            for nbr_x, nbr_y in points:
+                if x == nbr_x and y == nbr_y:
+                    continue
+                heapq.heappush(pq, (abs(x - nbr_x) + abs(y - nbr_y), x, y, nbr_x, nbr_y))
+
+        start_x, start_y = points[0][0], points[0][1] 
+        in_mst = {(start_x, start_y)}
+        add_edges(start_x, start_y)
+        
+        while len(in_mst) < n:
+            cost, fr_x, fr_y, to_x, to_y = heapq.heappop(pq)
+            if (to_x, to_y) in in_mst:
+                continue
+            
+            min_cost += cost
+            in_mst.add((to_x, to_y))
+
+            add_edges(to_x, to_y)
+
+        return min_cost
+        
