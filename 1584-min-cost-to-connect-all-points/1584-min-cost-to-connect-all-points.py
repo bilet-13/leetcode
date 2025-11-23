675d06e3c3082d783f14196cdea13186c6993b04
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-11-23 11:52:56 -0800

    Time: 511 ms (89.3%), Space: 85.4 MB (54.8%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,28 +1,35 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         pq = []
-        n = len(points)
         min_cost = 0
+        n = len(points)
+        parent = [i for i in range(n)]
+        in_mst_edge_num = 0
 
-        def add_edges(x, y):
-            for nbr_x, nbr_y in points:
-                if x == nbr_x and y == nbr_y:
-                    continue
-                heapq.heappush(pq, (abs(x - nbr_x) + abs(y - nbr_y), x, y, nbr_x, nbr_y))
+        for i in range(n):
+            fr_x, fr_y = points[i]
+            for j in range(i + 1, n):
+                to_x, to_y = points[j]
+                heapq.heappush(pq, (abs(fr_x - to_x) + abs(fr_y - to_y), i, j))
 
-        start_x, start_y = points[0][0], points[0][1] 
-        in_mst = {(start_x, start_y)}
-        add_edges(start_x, start_y)
-        
-        while len(in_mst) < n:
-            cost, fr_x, fr_y, to_x, to_y = heapq.heappop(pq)
-            if (to_x, to_y) in in_mst:
+        def find(x):
+            while x != parent[x]:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return parent[x]
+
+        def union(x, y):
+            parent[find(x)] = parent[find(y)]
+
+        while in_mst_edge_num < n - 1:
+            cost, fr, to = heapq.heappop(pq)
+
+            if find(fr) == find(to):
                 continue
-            
+
+            in_mst_edge_num += 1
             min_cost += cost
-            in_mst.add((to_x, to_y))
-
-            add_edges(to_x, to_y)
-
+            union(fr, to)
+        
         return min_cost
         
