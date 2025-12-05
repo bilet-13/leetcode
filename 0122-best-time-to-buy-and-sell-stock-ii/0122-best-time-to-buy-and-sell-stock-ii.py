970dad2a43442a43091e149c2e743dbfa1025887
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-12-05 10:54:55 -0800

    Time: 4 ms (20.08%), Space: 19 MB (29.07%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,6 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        total_profit = 0
+          # if you wnat to buy you need to have no share
+        # if we paint the price as 2d xy plane graph
+        # we can see that the maximum profit is the sum of the diffence between the two element xi, xj where j > i
+        # and xj > xi
+
+        max_result = 0
+
         for i in range(1, len(prices)):
-            total_profit += max(prices[i]-prices[i-1], 0)
-        return total_profit
+            max_result += max(0, prices[i] - prices[i - 1])
+        
+        return max_result
+            
+        
+
+
+
+
