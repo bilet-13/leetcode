83447e47d62dc0c9ee856ca4b2e87b4885585ca2
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-12-20 11:42:28 -0800

    Time: 43 ms (5.22%), Space: 19 MB (99.65%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,20 +1,24 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+         # brute force
+        # for each bar, its water is determin by the smallest neighbor among right and left tallest neighbor
+        result = 0
+        leftmost = [0]
+        rightmost = [0 for _ in range(len(height))]
 
-        left_max = [0 for _ in range(len(height))]
-        right_max = [0 for _ in range(len(height))]
-        max_element = height[0]
-        water = 0
+        for i in range(1, len(height)):
+            cur_left_most = max(height[i - 1], leftmost[i - 1])
+            leftmost.append(cur_left_most)
 
-        for i in range(len(height)):
-            max_element = max(max_element, height[i])
-            left_max[i] = max_element
-
-        max_element = height[-1]
-        for i in range(len(height)-1, -1, -1):
-            max_element = max(max_element, height[i])
-            right_max[i] = max_element
+        for i in range(len(height) - 2, -1, -1):
+            cur_right_most = max(height[i + 1], rightmost[i + 1])
+            rightmost[i] = cur_right_most
         
+        trap_water = 0
         for i in range(len(height)):
-            water += max(min(left_max[i], right_max[i]) - height[i], 0)
-        return water
+            trap_water += max(min(rightmost[i], leftmost[i]) - height[i], 0)
+
+        return trap_water
+
+        
+        
