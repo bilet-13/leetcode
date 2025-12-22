d3bf5d577879346afdfadfa582ba7817f84a7bb6
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2025-12-22 13:40:15 -0800

    Time: 0 ms (100%), Space: 18.7 MB (98.27%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,21 +1,15 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-             # two pointer
-        # so the sliding window size is k
-        # hash map to store the key: index and val: the close point == abs(a - x) for any a
-        # move sliding window until the new element is not closet compare to leftover elemtn
-        # when stop use return arr[left: left + k] 
         left = 0
-        result_left = 0
-        cur_closest_sum = sum(abs(num - x) for num in arr[:k])
-        min_closest_sum = cur_closest_sum 
+        right = len(arr) - k
 
-        for right in range(k, len(arr)):
-            cur_closest_sum += abs(arr[right] - x) - abs(arr[right - k] - x)
+        while left < right:
+            mid = (right + left) // 2
 
-            if cur_closest_sum < min_closest_sum:
-                min_closest_sum = cur_closest_sum
-                result_left = right - k + 1
-        
-        return arr[result_left: result_left + k] 
-        
+            if (arr[mid] + arr[mid + k]) / 2 >= x:
+                right = mid
+            else:
+                left = mid + 1
+
+        return arr[left: left + k]
+
