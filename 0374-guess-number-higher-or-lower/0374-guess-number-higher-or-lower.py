27147a18de6fd6519360326786be3c544ef35c2e
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-01-04 10:56:48 -0800

    Time: 30 ms (90.35%), Space: 17.5 MB (76.61%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -12,21 +12,18 @@ class Solution:
         left = 1
         right = n
 
-        while left <= right:
+        while left < right:
             mid = (right + left) // 2
 
             guess_result = guess(mid)
 
-            if guess_result == 0:
-                return mid
-            
-            elif guess_result == -1:
-                right = mid - 1
-
-            else:
+            if guess_result ==  1:
                 left = mid + 1
 
-        return -1
+            else:
+                right = mid
+
+        return left
 
 
         
