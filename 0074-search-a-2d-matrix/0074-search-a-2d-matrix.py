839c61e9a03d4d93cb3e36a6766f4974bea56447
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-01-08 15:00:26 -0800

    Time: 0 ms (100%), Space: 19.6 MB (6.1%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,18 +1,49 @@
 class Solution:
-
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        left = 0
-        right = len(matrix) * len(matrix[0]) - 1
+        m = len(matrix)
+        n = len(matrix[0])
 
-        while left <= right:
-            middle = (left + right) // 2
+        def find_row():
+            left = 0
+            right = m - 1
 
-            element = matrix[middle // len(matrix[0])][middle % len(matrix[0])]
-            if target == element:
-                return True
-            elif target > element:
-                left = middle + 1
-            else :
-                right = middle - 1
+            while left <= right:
+                mid = (right + left) // 2
 
-        return False
+                if matrix[mid][0] <= target <= matrix[mid][-1]:
+                    return mid
+                
+                elif target > matrix[mid][-1]:
+                    left = mid + 1
+                
+                else:
+                    right = mid - 1
+
+            return -1
+
+
+        def find_column(row):
+            if row < 0 or row >= m:
+                return False
+            
+            left = 0
+            right = n - 1
+
+            while left <= right:
+                mid = (right + left) // 2
+
+                if matrix[row][mid] == target:
+                    return True
+                
+                elif target > matrix[row][mid]:
+                    left = mid + 1
+                
+                else:
+                    right = mid - 1
+
+            return False
+
+        row = find_row()
+        return find_column(row)
+        
+        
