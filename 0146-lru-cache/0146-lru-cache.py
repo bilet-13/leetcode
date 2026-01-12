6a85b8d4cdab50fe371ee48b778f157eb34009ac
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-01-12 11:36:59 -0800

    Time: 132 ms (60.16%), Space: 76.9 MB (98.28%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,12 +1,23 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
         
-
     def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
         
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
+        if key not in self.cache and len(self.cache) == self.capacity:
+                self.cache.popitem(last=False)
+
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+
         
 
 
