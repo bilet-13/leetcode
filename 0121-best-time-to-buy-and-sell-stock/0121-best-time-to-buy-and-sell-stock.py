f91f96faf72e6ee7c368e2b5ae356800da8410d8
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-01-12 14:47:54 -0800

    Time: 127 ms (28.15%), Space: 29.1 MB (5.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # for each value, compute the difference between the value and its right largest neighbor
+        # return the max difference == the maximum profit
+        # time complexity o(n) 
+        # find all right smallest neighbors o(n) one pass
+        largest_rights = [p for p in prices]
 
-        min_price_before = prices[0]
-        max_profit = 0
+        for i in range(len(largest_rights) - 2, - 1, -1):
+            largest_rights[i] = max(prices[i + 1], largest_rights[i + 1]) 
 
-        for price in prices:
-            if (price - min_price_before > max_profit):
-                max_profit = price - min_price_before
+        result = 0
 
-            min_price_before = price if price < min_price_before else min_price_before
+        for i in range(len(prices)):
+            max_profit_i = largest_rights[i] - prices[i]
+            result = max(result, max_profit_i)
 
-        return max_profit
+        return result
+
+        
         
