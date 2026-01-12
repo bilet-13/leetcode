e900a0e89f7cb610daf289fc4f60987fbb3002a2
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-01-12 15:27:04 -0800

    Time: 3 ms (27.7%), Space: 19.4 MB (8.24%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,16 +1,17 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # strict prefix? like bat is not the strict prefix of bat
-        # normal prefix
-        commonPrefix = strs[0]
+        prefix = strs[0]
 
         for i in range(1, len(strs)):
-            commonPrefix = commonPrefix[:min(len(commonPrefix), len(strs[i]))]
+            s = strs[i]
 
-            for j in range(len(commonPrefix)):
-                if strs[i][j] != commonPrefix[j]:
-                    commonPrefix = commonPrefix[:j]
+            new_prefix = ""
+            for j in range(min(len(prefix), len(s))):
+                if prefix[j] != s[j]:
                     break
 
-        return commonPrefix
+                new_prefix += prefix[j]
+            prefix = new_prefix
+
+        return prefix
         
