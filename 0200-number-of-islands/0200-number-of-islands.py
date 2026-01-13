1461a8eee5678d4bae58aa1f29e3e86532a162d3
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-01-13 15:10:21 -0800

    Time: 235 ms (77.89%), Space: 21.7 MB (37.93%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # travsal every unvisited cell do BFS and 
-        # mark the vistied cell to 2
-        # use var to count the num of island
-        count = 0
+        # for each land , do bfs to find its correspond island and mark the visited cell to prevent 
+        # bfs again
+        # time complexity o(mn)
+
+        island_num = 0
         m = len(grid)
         n = len(grid[0])
 
-        def BFS(grid, i, j):
-            dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-            queue = deque()
-            queue.append((i, j))
-            grid[i][j] = '2'
+        def bfs(start_x, start_y):
+            queue = deque([(start_x, start_y)])
+            grid[start_x][start_y] == '2' # prevent revisiting
 
             while queue:
                 x, y = queue.popleft()
 
-                for dx, dy in dirs:
+                for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                     nx = x + dx
                     ny = y + dy
 
@@ -24,11 +23,10 @@ class Solution:
                         grid[nx][ny] = '2'
                         queue.append((nx, ny)) 
 
-        for i in range(len(grid)):
+        for i in range(len(grid)): # is the list may be empty?
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    count += 1
-                    BFS(grid, i, j)
-
-        return count
+                    bfs(i, j)
+                    island_num += 1
         
+        return island_num
