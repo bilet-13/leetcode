191d3e026d833198c93f0d236fb87e0829181be0
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-01-13 16:15:12 -0800

    Time: 0 ms (100%), Space: 19.4 MB (9.25%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -6,7 +6,7 @@ class Solution:
         # swap along x asix and y asix
         n = len(matrix)
         for i in range(n):
-            for j in range(i + 1, n):
+            for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
         for row in matrix:
