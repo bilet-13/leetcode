481d60b5157442b9e121ee96817a046c889a5524
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-02-03 22:29:07 -0800

    Time: 0 ms (100%), Space: 19.2 MB (50.43%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,14 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # double pointer o(n)
-        # first pointer : index of result
-        # second pointer : index of origin nums
-        non_val_idx = 0
+        result_idx = 0
 
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[non_val_idx] = nums[i]
-                non_val_idx += 1
+        for num in nums:
+            if num != val:
+                nums[result_idx] = num
+                result_idx += 1
 
-        return non_val_idx
-
+        return result_idx
