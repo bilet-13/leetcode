ee19c78e355e239ecd6e6615c38e1972edb87ff8
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-02-11 14:19:57 -0800

    Time: 1 ms (46.82%), Space: 19.5 MB (55.46%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,21 +1,31 @@
 class Solution:
-    def _find_permute(self, nums, start, result):
-        if start == len(nums):
-            result.append(nums[:])
-            return
-
-        for i in range(start, len(nums)):
-            nums[start], nums[i] = nums[i], nums[start]
-
-            self._find_permute(nums, start+1, result)
-
-            nums[start], nums[i] = nums[i], nums[start]
-
     def permute(self, nums: List[int]) -> List[List[int]]:
+        # backtraking
+        # cur: the list to record numbers we choosed
+        # start: define the start idx
+
+        # base case: when length of cur == length of nums add the cur to result and return
+
+        # else: start from idx and swap the number we choose
+
+
+        def backtrack(cur, start, result):
+            if start == len(nums):
+                result.append(cur[:])
+                return
+
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]# swap to make permutation
+                cur.append(nums[start])
+
+                backtrack(cur, start + 1, result)
+
+                cur.pop()
+                nums[start], nums[i] = nums[i], nums[start]
+
         result = []
-
-        self._find_permute(nums, 0, result)
-
+        backtrack([], 0, result)
+        
         return result
-
+        
     
