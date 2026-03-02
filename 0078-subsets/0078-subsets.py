438a584acc855e3ca71d58bb336651b61118306b
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-03-02 14:34:13 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,19 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        total_subsets = 1 << n
+        total_subsets = pow(2, n)
         result = []
 
         for i in range(total_subsets):
             current_subset = []
             for j in range(n):
-                if (i >> j) & 1:
+                if i  & (1 << j) != 0:
                     current_subset.append(nums[j])
 
             result.append(current_subset)
         
         return result
-        
+
 
 
         
