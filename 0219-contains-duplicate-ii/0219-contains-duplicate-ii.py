65c3850bff5ac1e960040168a1fb8a12127b1948
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-03-05 20:59:58 -0800

    Time: 76 ms (6.57%), Space: 35.3 MB (59.32%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,17 +1,21 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+         # sliding window keep the window size <= k
         left = 0
         nums_in_window = defaultdict(int)
 
         for right in range(len(nums)):
-            while left < right - k:
+            nums_in_window[nums[right]] += 1
+
+            while right - left > k:
                 nums_in_window[nums[left]] -= 1
                 left += 1
 
-            if nums_in_window[nums[right]] > 0:
-                return True 
-
-            nums_in_window[nums[right]] += 1
-            
+            if nums_in_window[nums[right]] == 2:
+                return True  
+        
         return False
+
+        
+
         
