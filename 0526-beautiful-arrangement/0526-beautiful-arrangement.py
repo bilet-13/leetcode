2015bf5eb678e666561f04b7ddf875f7153819e5
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-03-06 16:00:34 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -1,32 +1,27 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
-        # backtracking do permutaion and check rules in each level
+        # dping do permutaion and check rules in each level
         # input argument: index to track
         # if index == n: add the permuationt
         # else 
-        # check [index] is true and do backtrack(idx + 1)
+        # check [index] is true and do dp(idx + 1)
         nums = [i for i in range(1, n + 1)]
-        mask = 0
-        result = 0
 
-        def backtrack(idx):
-            nonlocal result
-            nonlocal mask
+        @cache
+        def dp(mask, idx):
             if idx == 0:
-                result += 1
-                return
+                return 1
+
+            total_arrangement = 0
 
             for num in range(1, n + 1):
                 if mask & (1 << num) == 0 and (num % idx == 0 or idx % num == 0):
+                    total_arrangement += dp(mask | (1 << num), idx - 1)
 
-                    mask |= 1 << num
+            return total_arrangement
 
-                    backtrack(idx - 1)
+        return dp(0, n)
 
-                    mask ^= 1 << num
-
-        backtrack(n)
-        return result
                 
 
         
