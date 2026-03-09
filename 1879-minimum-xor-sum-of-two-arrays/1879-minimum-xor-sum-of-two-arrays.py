0328e278b289cb487b22a7aef46910ed47f7d6d0
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-03-09 15:34:35 -0700

    Time: 262 ms (27.72%), Space: 39.2 MB (20.79%) - LeetHub

---
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
+        # brute force: use backtracking to find permuatitons
+        #  return the minimual XOR Sum of nums1 and one of the permuaitons of nums2
+        # beacuse the probiem fit the makov property -> so can use dp/memo
+        minimum_xor_sum = float("inf")
+        n = len(nums1)
+
+        def get_xor_sum(nums2):
+            xor_sum = 0
+
+            for i in range(len(nums2)):
+                xor_sum += nums1[i] ^ nums2[i]
+            
+            return xor_sum
+
+        @cache
+        def dp(mask, cur_idx):
+            if cur_idx == n:
+                return 0
+
+            min_subproblem_sum = float("inf")
+
+            for i in range(n):
+                if mask & (1 << i) == 0:
+                    xor_sum = nums1[cur_idx] ^ nums2[i]
+
+                    sub_result = dp(mask | (1 << i),  cur_idx + 1)
+
+                    min_subproblem_sum = min(min_subproblem_sum, xor_sum + sub_result)
+
+            return min_subproblem_sum
+
+        return dp(0, 0)
+
+
+
+
+
+        
