514d3f38cfb670b3cd46ab4d89922df23620e96e
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-03-12 16:35:04 -0700

    Time: 311 ms (7.46%), Space: 56.6 MB (45.03%) - LeetHub

---
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.py
@@ -0,0 +1,40 @@
+class Solution:
+    def numberWays(self, hats: List[List[int]]) -> int:
+        # mask dp
+        # mask: ith bit is 1 if ith person already wear hat
+        #
+        n = len(hats)
+        hats_people = defaultdict(set)
+        hats_num = 40
+
+        for p in range(n):
+            for hat in hats[p]:
+                hats_people[hat].add(p)
+       
+        @cache
+        def dp(hat_id, mask):
+            cur_idx = mask.bit_count()
+
+            if cur_idx == n:
+                return 1
+
+            if hat_id > hats_num:
+                return 0
+
+            ways = 0
+
+            for p in range(n):
+                if mask & (1 << p) == 0 and p in hats_people[hat_id]:
+                    ways += dp(hat_id + 1, mask | (1 << p))
+
+            ways = (ways + dp(hat_id + 1, mask)) % (10**9 + 7)
+
+            return ways
+
+        return dp(0, 0)
+
+
+
+
+            
+        
