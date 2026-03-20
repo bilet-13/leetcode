23f51219aa3abf9cd51c1341529ac2a0507ec828
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-03-19 21:20:34 -0700

    Time: 7 ms (89.84%), Space: 22.6 MB (71.2%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,19 +1,24 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        # so the town judge would be the  condition: node that indegree is n - 1  and out degree is 0 
+        # the edge is define ai -> bi when trust[i] = [ai, bi]
+        # so just check if there is only one node satify the condiiton
+        # time complexity o(n + len(trust)) at most o(n ^ 2)
         in_degrees = [0 for _ in range(n + 1)]
         out_degrees = [0 for _ in range(n + 1)]
 
-        for person_a, person_b in trust:
-            in_degrees[person_b] += 1
-            out_degrees[person_a] += 1
-        
-        people_everybody_trust = []
+        for a, b in trust:
+            in_degrees[b] += 1
+            out_degrees[a] += 1
+
+        town_judge = -1
 
         for i in range(1, n + 1):
-            if in_degrees[i] == n - 1:
-                people_everybody_trust.append(i)
+            if in_degrees[i] == n - 1 and out_degrees[i] == 0:
+                if town_judge != -1:
+                    return -1 # multiple answer
+
+                town_judge = i
+
+        return town_judge
         
-        if len(people_everybody_trust) != 1:
-            return -1
-        
-        return people_everybody_trust[0] if out_degrees[people_everybody_trust[0]] == 0 else -1 
