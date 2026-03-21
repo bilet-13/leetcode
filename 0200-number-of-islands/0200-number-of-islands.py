5c04ac9adfae811585423394efafa63da8fe3ed1
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-03-20 21:34:41 -0700

    Time: 251 ms (43.21%), Space: 21.6 MB (85.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,33 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # for each land , do bfs to find its correspond island and mark the visited cell to prevent 
-        # bfs again
-        # time complexity o(mn)
+        # dfs: start from 1 cell and mark the land cell to water
+        # use a global var to store the number of islands
 
-        island_num = 0
-        m = len(grid)
-        n = len(grid[0])
+        num_islands = 0
+        n = len(grid)
+        m = len(grid[0])
 
-        def bfs(start_x, start_y):
-            queue = deque([(start_x, start_y)])
-            grid[start_x][start_y] == '2' # prevent revisiting
+        def dfs(s_x, s_y):
+            stack = [(s_x, s_y)]
+            grid[s_x][s_y] = "0"
 
-            while queue:
-                x, y = queue.popleft()
+            while stack:
+                x, y = stack.pop()
 
-                for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                     nx = x + dx
                     ny = y + dy
 
-                    if m > nx >= 0 and n > ny >= 0 and grid[nx][ny] == '1':
-                        grid[nx][ny] = '2'
-                        queue.append((nx, ny)) 
+                    if n > nx >= 0 and m > ny >= 0 and grid[nx][ny] == "1":
+                        grid[nx][ny] = "0"
+                        stack.append((nx, ny))
 
-        for i in range(len(grid)): # is the list may be empty?
+        for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    bfs(i, j)
-                    island_num += 1
+                if grid[i][j] == "1":
+                    num_islands += 1
+                    dfs(i, j)
+
+        return num_islands
+
         
-        return island_num
