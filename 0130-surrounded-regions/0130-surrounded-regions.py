b83d0c155a06eefae1e7303268ba8dad9f66feb2
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-04-01 22:22:55 -0700

    Time: 570 ms (5.27%), Space: 27.6 MB (12.62%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,43 +3,42 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-          #         modify those cell to X
-        m = len(board)
-        n = len(board[0])
-        visited = [[False for _ in range(n)] for _ in range(m)] 
+          # not surround region -> the groupd that has 'O' on an edge
+        # so change regions that don't have o on an edge
+        # dfs for  
+        
+        def dfs(start_x, start_y):
+            stack = [(start_x, start_y)]
+            visited = set(stack)
 
-        def BFS(r, c):
-            # travser the O group
-            # mark the visited cell if can not reach board
-            queue = deque([(r, c)])
-            visited[r][c] = True
-            isSurround = True
-            visited_cell = []
+            n = len(board)
+            m = len(board[0])
+            surrounded = True
 
-            while queue:
-                x, y = queue.popleft()
+            while stack:
+                cur_x, cur_y = stack.pop()
+
+                if cur_x == 0 or cur_x == n - 1 or cur_y == 0 or cur_y == m - 1:
+                    surrounded = False
                 
-                visited_cell.append((x, y))   
-                if x == 0 or x == m - 1 or y == 0 or y == n - 1:
-                   isSurround = False 
+                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    nx = cur_x + dx
+                    ny = cur_y + dy
 
-                for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                    nx = x + dx
-                    ny = y + dy
+                    if n > nx >= 0 and m > ny >= 0 and board[nx][ny] == 'O' and (nx, ny) not in visited:
+                        visited.add((nx, ny))
+                        stack.append((nx, ny))
 
-                    if m > nx >= 0 and n > ny >= 0 and not visited[nx][ny] and board[nx][ny] == 'O':
-                        visited[nx][ny] = True
-                        queue.append((nx, ny))
+            if surrounded:
+                for x, y in visited:
+                    board[x][y] = 'X'
 
-            if isSurround:
-                for i, j in visited_cell:
-                    board[i][j] = 'X'
-                    
-            return
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 'O':
+                    dfs(i, j)
 
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == 'O' and not visited[i][j]:
-                    BFS(i, j)
         return
+        
         
