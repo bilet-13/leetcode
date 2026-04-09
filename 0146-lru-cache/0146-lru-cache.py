6b4d903484c59818f8224ff88c1df77ef0f21e3b
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-04-09 10:58:28 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,22 +1,27 @@
+from collections import OrderedDict
 class LRUCache:
+    # Orderdict
+    # move_to_end
+    # popitem
+    # time complexity o(1)
 
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
         self.capacity = capacity
-        
+        self.map = OrderedDict()
+
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key not in self.map:
             return -1
-        
-        self.cache.move_to_end(key)
-        return self.cache[key]
+
+        self.map.move_to_end(key, last=False)
+        return self.map[key]
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.cache and len(self.cache) == self.capacity:
-                self.cache.popitem(last=False)
-
-        self.cache[key] = value
-        self.cache.move_to_end(key)
+        if len(self.map) == self.capacity:
+            self.map.popitem()
+        
+        self.map[key] = value
+        self.map.move_to_end(key, last=False)
 
         
 
