3504bf2b9330ca0f9f53a754caece79d76612a71
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-04-11 11:35:37 -0700

    Time: 1404 ms (41.94%), Space: 24.9 MB (69.44%) - LeetHub

---
+++ 3434-maximum-frequency-after-subarray-operation/3434-maximum-frequency-after-subarray-operation.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        # dp?
+        # dp[i][j]: the maximum frequencey of any number of the subarr[i: j]
+        # brute force
+        # two for loop for i  and for j , count the number of max frequency val
+        # 
+
+        counter = Counter(nums)
+        result = 0
+        k_num = counter[k] if k in counter else 0
+
+        for num, count in counter.items():
+            if num == k:
+                result = max(result, count)
+            else:
+                cur = 0
+                max_cur = 0
+                for i in range(len(nums)):
+                    cur = max(cur, 0)
+
+                    if nums[i] == k:
+                        cur -= 1
+                    elif nums[i] == num:
+                        cur += 1
+
+                    max_cur = max(cur, max_cur)
+
+                result = max(result, max_cur + k_num) 
+
+        return result
+
+                    
+
+
