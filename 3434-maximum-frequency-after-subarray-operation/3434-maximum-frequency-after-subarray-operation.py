f29a3b743b4d475f673811f357e209b12044dc87
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-04-11 11:42:46 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3434-maximum-frequency-after-subarray-operation/3434-maximum-frequency-after-subarray-operation.py
+++ 3434-maximum-frequency-after-subarray-operation/3434-maximum-frequency-after-subarray-operation.py
@@ -6,27 +6,27 @@ class Solution:
         # two for loop for i  and for j , count the number of max frequency val
         # 
 
+        # time complexity o(V* len(nums))
+
         counter = Counter(nums)
         result = 0
         k_num = counter[k] if k in counter else 0
 
         for num, count in counter.items():
-            if num == k:
-                result = max(result, count)
-            else:
-                cur = 0
-                max_cur = 0
-                for i in range(len(nums)):
-                    cur = max(cur, 0)
+          
+            cur = 0
+            max_profit = 0
+            for i in range(len(nums)):
+                cur = max(cur, 0)
 
-                    if nums[i] == k:
-                        cur -= 1
-                    elif nums[i] == num:
-                        cur += 1
+                if nums[i] == k:
+                    cur -= 1
+                elif nums[i] == num:
+                    cur += 1
 
-                    max_cur = max(cur, max_cur)
+                max_profit = max(cur, max_profit)
 
-                result = max(result, max_cur + k_num) 
+            result = max(result, max_profit + k_num) 
 
         return result
 
