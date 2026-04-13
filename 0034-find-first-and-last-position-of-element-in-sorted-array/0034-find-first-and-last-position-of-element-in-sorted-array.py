c159767b20b9c2e24a34ceee8750e64b0d9e838d
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-04-13 11:10:57 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,54 +1,41 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # do binary search twice                               V  
+        # first binary search condiciton : num >= target x x x o o o 
 
+       # do binary search twice                               V  
+        # second binary search condiciton : num > target, x x x o o o 
         if not nums:
-	        return [-1, -1]
+            return [-1, -1]
+        
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] >= target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        if left >= len(nums) or nums[left] != target:
+            return [-1, -1]
+
+        result = [left]
 
         left = 0
         right = len(nums) - 1
-        first_index_tar = -1
-        
-        while left < right:
+
+        while left <= right:
             mid = (left + right) // 2
-            
+
             if nums[mid] > target:
-                right = mid
-            elif nums[mid] < target:
-                left = mid + 1
+                right = mid - 1
             else:
-                if mid - 1 >= 0 and nums[mid-1] == target:
-                    right = mid
-                else:
-                    first_index_tar = mid
-                    break
-        else:
-            if nums[left] == target:
-                first_index_tar = left
-
-        if first_index_tar == -1:
-            return [-1, -1]
-
-        left = first_index_tar
-        right = len(nums) - 1
-        last_index_tar = -1
+                left = mid + 1
         
-        while left < right:
-            mid = (left + right) // 2
-            
-            if nums[mid] > target:
-                right = mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                if mid + 1 < len(nums) and nums[mid+1] == target:
-                    left = mid + 1
-                else:
-                    last_index_tar = mid
-                    break
-        else:
-            if nums[left] == target:
-                last_index_tar = left
+        result.append(right)
 
-        return [first_index_tar, last_index_tar]
-
+        return result
         
