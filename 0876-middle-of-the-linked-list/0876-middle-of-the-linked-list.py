e90a222442ceb3214d193bd96750417d98af5dd1
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-04-16 10:19:43 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -6,11 +6,13 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # slow, fast pointer
-        # slow took one step and fast took two steps each time untile fast reach the end or it can not jump twice and check if there is still a node if yes return 
+        # slow took one step and fast took two steps each time untile fast reach the end or it can not jump twice and check if there is still a one node if yes return slow.next else slow
         # 1 2 3 4 5 
         # start from 1 fast 1 3 5, slow 1 2 3 return slow
         # 1 2 3 4 5 6        
         # start from 1 fast 1 3 5, slow 1 2 3  return slow.next
+        if not head:
+            return head
 
         fast = head
         slow = head
