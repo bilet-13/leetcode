1e233e80549ab659440451c4e890d917f21331d3
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-04-16 09:29:56 -0700

    Time: 44 ms (16.57%), Space: 21.9 MB (64.16%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,18 +1,25 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # time complexity o(n)
-        prefix_count = defaultdict(int)
-        prefix_count[0] = 1
-        count = 0
+        # integer, negative , positive
+        # prefix sum
+        # k = prefix[i] - prefix[j] i > j
+        # prefix[j] = prefix[i] - k
+        # record the number of prefix[j]
+        #time comolexity o(n)
+        #space comolexity o(n)
+
+        prefix_sum = defaultdict(int)
+        prefix_sum[0] = 1
         cur_sum = 0
+        result = 0
 
         for num in nums:
             cur_sum += num
+            target = cur_sum - k
 
-            target_prefix_sum = cur_sum - k
+            result += prefix_sum.get(target, 0)
+            prefix_sum[cur_sum] += 1
 
-            count += prefix_count[target_prefix_sum]
+        return result
 
-            prefix_count[cur_sum] += 1
 
-        return count
