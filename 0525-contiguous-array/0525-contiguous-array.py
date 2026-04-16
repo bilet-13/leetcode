4a9a0f6f89786046517e99c0a34248c2e4a94cf2
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-04-16 15:47:13 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -7,17 +7,19 @@ class Solution:
 
         # 0 = cur_sum - prefix[j] the farthest j
         # hash map: key : sum value the leftmost idx
-        new_nums = [1 if nums[i] == 1 else -1 for i in range(len(nums))]
+        new_nums = [-1 if num == 0 else 1 for num in nums]
         prefix_sum = {0: 0}
         cur_sum = 0
         max_len = 0
 
         for i in range(len(nums)):
             cur_sum += new_nums[i]
+            cur_len = i + 1
+
             if cur_sum in prefix_sum:
-                max_len = max(max_len, i - prefix_sum[cur_sum] + 1)
+                max_len = max(max_len, cur_len - prefix_sum[cur_sum])
             else:
-                prefix_sum[cur_sum] = i + 1
+                prefix_sum[cur_sum] = cur_len 
         
         return max_len
             
