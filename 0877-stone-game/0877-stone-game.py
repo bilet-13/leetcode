2a3d8e6d9f3e019e134ad3879e68452addb323e0
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-08-08 19:24:54 -0700

    Time: 143 ms (23.77%), Space: 25.5 MB (23.2%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.py
@@ -0,0 +1,27 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        # dp[i][j] = max positive diffrence if i take first of the arr[i:j]
+        n = len(piles)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = piles[i]
+
+        for length in range(2, n + 1):
+
+            for i in range(n - length + 1):
+                j = i + length - 1
+
+                dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1])
+
+        return dp[0][n - 1] > 0
+            
+
+
+
+
+
+
+
+
+
